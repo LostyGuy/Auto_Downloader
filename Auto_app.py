472140dc97f4ab9b3813a90137fdfb4de252f1ef
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2023-11-03 19:00:26 +0100

    List
    Made lists for easier access and removed some unnecessary things

--- Auto_app.py
+++ Auto_app.py
@@ -6,34 +6,40 @@ import urllib.request
 
 obs_url = "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-29.1.3-Full-Installer-x64.exe"
 obs_filename = ""
-OBS = {obs_url, obs_filename}
+OBS = {'url' : obs_url,'filename' : obs_filename}
 
 notepad_url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.8/npp.8.5.8.Installer.x64.exe"
 notepad_filename = ""
-NOTEPAD = {notepad_url, notepad_filename}
+NOTEPAD = {'url' : notepad_url,'filename' :  notepad_filename}
 
 CppDEV_url = "https://downloads.sourceforge.net/project/dev-cpp/Binaries/Dev-C%2B%2B%204.9.9.2/devcpp-4.9.9.2_setup.exe?ts=gAAAAABlRSjXezyGj2HfunrUQ2Lc-9-7g0vs7RCd41BglxwijThpbLU6W33t58YXRZBSeSLFHPGm_MTC5qkzY3HaIrBEBS4iOQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdev-cpp%2Ffiles%2FBinaries%2FDev-C%252B%252B%25204.9.9.2%2Fdevcpp-4.9.9.2_setup.exe%2Fdownload"
 CppDEV_filename = ""
-CppDEV = {CppDEV_url, CppDEV_filename}
+CppDEV = {'url' : CppDEV_url,'filename' :  CppDEV_filename}
 
 #get User-Agent automatically
 headers = {
 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0'
 }
 
-global g_output
-g_output = {False, False, False}
+g_input = {}
 
 def menu():
 
     print("options 1 - OBS,2 - NOTEPAD,3 - C++DEV")
-    i = input("choose your Pokemon!")
+    i = int(input("choose your Pokemon!"))
+    if i == 1:
+        g_input.update(OBS)
+    elif i == 2:
+        g_input.update(NOTEPAD)
+    elif i == 3:
+        g_input.update(CppDEV)
+    print(g_input)
 
 
 
 def request_file():
     
-
+    g_input[1]
 
 
     #sends request
@@ -42,5 +48,7 @@ def request_file():
     urllib.request.urlretrieve(request.full_url  )
 
 
-request_file()
+#request_file()
+menu()
 
+print(g_input)
