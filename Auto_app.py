4d3642cbec8027ec51a6b2017a7b1a702e660c68
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2023-11-03 18:24:46 +0100

    Cosmetics
    'cause I like them

--- Auto_app.py
+++ Auto_app.py
@@ -1,27 +1,39 @@
 import urllib.request
 
 #genshin_url = "https://sg-public-api.hoyoverse.com/event/download_porter/trace/ys_global/genshinimpactpc/default?url=https%3A%2F%2Fact.hoyoverse.com%2Fpuzzle%2Fhk4e%2Fpz_wY19_dy4do%2Findex.html%3Fpz_plat%3Dpc%26lang%3Den-us%26game_biz%3Dhk4e_global%26bridge_name%3Dpz_wY19_dy4do"
+#file_name = "Genshin_Impact_x64.exe"
 # non .exe URL - BIG problem
 
 obs_url = "https://cdn-fastly.obsproject.com/downloads/OBS-Studio-29.1.3-Full-Installer-x64.exe"
 obs_filename = ""
+OBS = {obs_url, obs_filename}
 
 notepad_url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v8.5.8/npp.8.5.8.Installer.x64.exe"
 notepad_filename = ""
+NOTEPAD = {notepad_url, notepad_filename}
 
 VSC_url = "https://downloads.sourceforge.net/project/dev-cpp/Binaries/Dev-C%2B%2B%204.9.9.2/devcpp-4.9.9.2_setup.exe?ts=gAAAAABlRSjXezyGj2HfunrUQ2Lc-9-7g0vs7RCd41BglxwijThpbLU6W33t58YXRZBSeSLFHPGm_MTC5qkzY3HaIrBEBS4iOQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdev-cpp%2Ffiles%2FBinaries%2FDev-C%252B%252B%25204.9.9.2%2Fdevcpp-4.9.9.2_setup.exe%2Fdownload"
 VSC_filename = ""
+VSC = {VSC_url, VSC_filename}
+
+#get User-Agent automatically
+headers = {
+'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0'
+}
+
+def menu():
+
+    if:
 
 def request_file():
-    file_name = "Genshin_Impact_x64.exe"
-    headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0'
-    }
+    
 
-    # Use urllib.request.urlretrieve to download the file
-    request = urllib.request.Request(obs_url, headers=headers)
 
-    urllib.request.urlretrieve(request.full_url, file_name)
+
+    #sends request
+    request = urllib.request.Request(, headers=headers)
+    #downloads file from request
+    urllib.request.urlretrieve(request.full_url, x)
 
 
 request_file()
