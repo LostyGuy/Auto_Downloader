b694e24a9192a37f97dcbba51a95fb5066e10672
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2023-11-04 19:24:10 +0100

    Script
    some if and elses and instalation commands

--- pip_play.py
+++ pip_play.py
@@ -1,12 +1,37 @@
 import os
 import subprocess
 
+complete_tqdm = 0
+complete_requests = 0
+
 def file_check():
+
     check_tqdm = "pip show tqdm"
     check_requests = "pip show requests"
 
-    out_tqdm = os.system(f'powershell.exe -Command "{check_tqdm}"')
-    print(os.system("powershell.exe -Command pip show me"))
-    should_tqdm = ""
+    out_tqdm = os.popen(f'powershell.exe -Command "{check_tqdm}"')
+    out_requests = os.popen(f'powershell.exe - Command "{check_requests}"')
+
+    #print(os.system("powershell.exe -Command pip show me"))
+
+    should_tqdm = "WARNING: Package(s) not found: tqdm"
+    should_requests = "WARNING: Package(s) not found: requests"
+
+    if out_tqdm == should_tqdm:
+        os.popen("powershell.exe -Command pip install tqdm")
+    else:
+        complete_tqdm.update(1)
+    
+    if out_requests == should_requests:
+        os.popen("powershell.exe - Command pip install requests")
+    else:
+        complete_requests.update(1)
+
+
 
 file_check()
+
+if complete_tqdm and complete_tqdm == 1:
+    os.system("start Auto_app.py")
+else:
+    file_check()
