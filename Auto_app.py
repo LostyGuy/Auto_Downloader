7ede6438fbb0b43575c22463748d9d567eabe600
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2023-11-04 14:41:48 +0100

    C++
    This URL is to difficult for me, I get a new one

--- Auto_app.py
+++ Auto_app.py
@@ -13,27 +13,27 @@ notepad_url = "https://github.com/notepad-plus-plus/notepad-plus-plus/releases/d
 notepad_filename = "NotePad++_x64.exe"
 NOTEPAD = {'url' : notepad_url,'filename' :  notepad_filename}
 
-CppDEV_url = "https://downloads.sourceforge.net/project/dev-cpp/Binaries/Dev-C%2B%2B%204.9.9.2/devcpp-4.9.9.2_setup.exe?ts=gAAAAABlRSjXezyGj2HfunrUQ2Lc-9-7g0vs7RCd41BglxwijThpbLU6W33t58YXRZBSeSLFHPGm_MTC5qkzY3HaIrBEBS4iOQ%3D%3D&r=https%3A%2F%2Fsourceforge.net%2Fprojects%2Fdev-cpp%2Ffiles%2FBinaries%2FDev-C%252B%252B%25204.9.9.2%2Fdevcpp-4.9.9.2_setup.exe%2Fdownload"
-CppDEV_filename = "C++DEV_x64.exe"
-CppDEV = {'url' : CppDEV_url,'filename' :  CppDEV_filename}
+steam_url = "https://cdn.akamai.steamstatic.com/client/installer/SteamSetup.exe"
+steam_filename = "Steam_x64.exe"
+STEAM = {'url' : steam_url,'filename' :  steam_filename}
 
 #get User-Agent automatically
 headers = {
-'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0'
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 OPR/102.0.0.0'
 }
 
 g_input = {}
 
 def menu():
 
-    print("options 1 - OBS,2 - NOTEPAD,3 - C++DEV")
-    i = int(input("choose your Pokemon!"))
+    print("options 1 - OBS,2 - NOTEPAD,3 - STEAM")
+    i = int(input("choose your Pokemon! "))
     if i == 1:
         g_input.update(OBS)
     elif i == 2:
         g_input.update(NOTEPAD)
     elif i == 3:
-        g_input.update(CppDEV)
+        g_input.update(STEAM)
 
 
 
@@ -51,7 +51,7 @@ def request_file():
     with tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, dynamic_ncols=True) as t:
         with urllib.request.urlopen(request) as response:
             with open(Filename, 'wb') as file:
-                total_size = int(response.info().get('Content-Length').strip())
+                total_size = int(response.info().get('Content-Length',0))
                 t.total = total_size
                 while True:
                     chunk = response.read(1024)
