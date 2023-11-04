d9e576742e4f60102decf8ab5e32a3e7d98e5f6e
LostyGuy <89598637+LostyGuy@users.noreply.github.com>
2023-11-04 20:37:57 +0100

    Sure
    Make sure that output is correct, clear in string changed ifs for better output, and is both are positive then it can open new file

--- pip_play.py
+++ pip_play.py
@@ -3,36 +3,46 @@ import subprocess
 
 complete_tqdm  = 0
 complete_requests = 0
+index = 0
 
 def file_check():
+
+    global index
 
     check_tqdm = "pip show tqdm"
     check_requests = "pip show requests"
 
-    out_tqdm = subprocess.run(['powershell.exe', '-Command', check_tqdm], capture_output=True, text=True, check=True).stdout
-    out_requests = subprocess.run(['powershell.exe', '-Command', check_requests], capture_output=True, text=True, check=True).stdout
+    out_tqdm = subprocess.run(['powershell.exe', '-Command', check_tqdm], capture_output=True, text=True, check=True, shell = True).stdout
+    out_requests = subprocess.run(['powershell.exe', '-Command', check_requests], capture_output=True, text=True, check=True, shell = True).stdout
 
     #print(os.system("powershell.exe -Command pip show me"))
 
     should_tqdm = "WARNING: Package(s) not found: tqdm"
     should_requests = "WARNING: Package(s) not found: requests"
 
-    print(out_tqdm, out_requests)
+    #print(out_tqdm, out_requests)
 
     if out_tqdm.strip() == should_tqdm:
         os.system("powershell.exe -Command pip install tqdm")
     else:
         complete_tqdm = 1
     
-    if out_requests.strip() == should_requests:
+    if out_requests.strip() in should_requests:
         os.system("powershell.exe - Command pip install requests")
     else:
         complete_requests = 1
+        
+    if complete_requests and complete_tqdm:
+         index = 1
+         return True
+    else:
+        return False
 
 
+file_name = "Auto_app.py"
 file_check()
 
-if (complete_tqdm and complete_requests == 1) or (complete_requests and complete_tqdm == 1):
-    os.system("start Auto_app.py")
+if index == 1:
+    os.system(f"start {file_name}")
 else:
     file_check()
